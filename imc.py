7b7a5321a4fcc7c1a335b6bd618d7f0cb7730d78
Felipe <diegoc.oliveira@gmail.com>
2016-11-10 17:06:09 -0300

    Programa bugado e errado

---
+++ imc.py
@@ -0,0 +1,7 @@
+massa 	= 	input("Digite sua Massa:")
+altura 	= 	input("Digite sua Altura:")
+if massa < 0 or altura < 0
+	print "Dados invalidos"
+else
+	imc     =       massa / altura**2
+	print   imc
